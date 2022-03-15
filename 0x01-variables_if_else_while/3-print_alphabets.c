8a274612dc85318a073bf8b4e354215108532e54
abby273 <adraphael2020@gmail.com>
2022-03-15 07:34:40 -0700

    uppercase and lowercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; c++)
+		putchar(k);
+	for (k = 'A'; k <= 'Z'; c++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
