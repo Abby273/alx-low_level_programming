84eac0b4df5acc0282d5c5e94ffba7667c7949d8
abby273 <adraphael2020@gmail.com>
2022-04-11 10:42:57 -0700

    macro named SIZE

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,14 @@
 #ifndef 0-OBJECT_LIKE_MACRO_H
 #define 0-OBJECT_LIKE_MACRO_H
+#include <stdio.h>
 
 #define SIZE 1024
 
+int main()
+{
+    int i = SIZE;
+    printf("%d\, i);
+
+    return (0);
+}
 #endif
